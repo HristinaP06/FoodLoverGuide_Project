2021fd2014f8e4e5ef973bf02d64f5a47a9fe6f8
HristinaP06 <hristina_pirinova@mail.bg>
2025-01-22 11:53:00 +0200

    Added Identity

--- FoodLoverGuide.DataAccess/ApplicationDbContext.cs
+++ FoodLoverGuide.DataAccess/ApplicationDbContext.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace FoodLoverGuide.DataAccess
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
     }
 }
