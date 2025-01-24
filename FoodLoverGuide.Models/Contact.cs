0e0f179f5e1b8d095a18062d7b4e047886b708e3
HristinaP06 <hristina_pirinova@mail.bg>
2025-01-24 15:42:11 +0200

    Added more information to models

--- FoodLoverGuide.Models/Contact.cs
+++ FoodLoverGuide.Models/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ namespace FoodLoverGuide.Models
 {
     public class Contact
     {
+        [Key]
         public Guid Id { get; set; }
         public string? Telephone { get; set; }
         public string? Email { get; set; }
--- FoodLoverGuide.Models/Reservation.cs
+++ FoodLoverGuide.Models/Reservation.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,17 @@ namespace FoodLoverGuide.Models
 {
     public class Reservation
     {
+        [Key]
         public Guid Id { get; set; }
+
+        [ForeignKey(nameof(Restaurant))]
+        public Guid RestaurantId { get; set; }
+        public Restaurant _Restaurant { get; set; }
+
+        public int? Adults { get; set; }
+        public int? Children { get; set; }
+
+        public DateTime Date { get; set; }
+
     }
 }
