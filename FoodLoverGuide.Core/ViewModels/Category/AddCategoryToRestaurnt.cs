59eae99153a0452740f67440f628ea2383bb663e
HristinaP06 <hristina_pirinova@mail.bg>
2025-02-18 23:46:10 +0200

    Added new View Model for Category

---
+++ FoodLoverGuide.Core/ViewModels/Category/AddCategoryToRestaurnt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodLoverGuide.Core.ViewModels.Category
+{
+    internal class AddCategoryToRestaurnt
+    {
+        public string CategoryName { get; set; }
+
+    }
+}
