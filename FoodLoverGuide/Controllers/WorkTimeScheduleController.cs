d42b561b049f99ebeb4d7a5b301039f93a82d432
HristinaP06 <hristina_pirinova@mail.bg>
2025-02-18 23:49:34 +0200

    Added controller for WorkTimeSchedule

---
+++ FoodLoverGuide/Controllers/WorkTimeScheduleController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Host;
+
+namespace FoodLoverGuide.Controllers
+{
+    public class WorkTimeScheduleController : Controller
+    {
+        private readonly IWorkspaceService workspaceService;
+
+        public WorkTimeScheduleController(IWorkspaceService workspaceService)
+        {
+            this.workspaceService = workspaceService;
+        }
+
+        public IActionResult IndexAsync()
+        {
+            return View();
+        }
+    }
+}
