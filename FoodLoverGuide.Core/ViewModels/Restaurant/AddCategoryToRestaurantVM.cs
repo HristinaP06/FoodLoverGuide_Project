7774bba20aee578ae8c785d3ff40aead83bbfdd5
HristinaP06 <hristina_pirinova@mail.bg>
2025-02-21 15:50:26 +0200

    Deleted one property from AddCategoryToRestaurantVM

--- FoodLoverGuide.Core/ViewModels/Restaurant/AddCategoryToRestaurantVM.cs
+++ FoodLoverGuide.Core/ViewModels/Restaurant/AddCategoryToRestaurantVM.cs
@@ -6,8 +6,6 @@ namespace FoodLoverGuide.Core.ViewModels.Restaurant
     {
         public Guid RestaurantId { get; set; }
 
-        public Models.Restaurant Restaurant { get; set; }
-
         public  List<Guid> SelectedCategoriesIds { get; set; } = new List<Guid>();
 
         public List<Category> CategoriesList { get; set; } = new List<Category>();
