86547731ca67a7763cda48bc8cac839b0511f4b7
HristinaP06 <hristina_pirinova@mail.bg>
2025-02-27 15:49:19 +0200

    Created view model for workTimeSchedule

---
+++ FoodLoverGuide.Core/ViewModels/Restaurant/AddWorkTimeScheduleToRestaurantVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodLoverGuide.Core.ViewModels.Restaurant
+{
+    public class AddWorkTimeScheduleToRestaurantVM
+    {
+        public Guid RestaurantId { get; set; }
+
+        public Dictionary<DayOfWeek, TimeSpan> WorkSchedule { get; set; }
+
+    }
+}
