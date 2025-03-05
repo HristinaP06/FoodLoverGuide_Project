d902a912d0a666ac6ca4b1db53357eafbedd0d75
HristinaP06 <hristina_pirinova@mail.bg>
2025-03-05 15:45:50 +0200

    Added new field in MenuItem and RestaurantPhoto

---
+++ FoodLoverGuide.DataAccess/Migrations/20250305093605_AddedNewFieldInRestaurantPhotoAndMenuItemEntities.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodLoverGuide.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNewFieldInRestaurantPhotoAndMenuItemEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Photo",
+                table: "RestaurantPhotos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Photo",
+                table: "RestaurantPhotos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
