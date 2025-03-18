75bb30b36835c5bd8d11ae8f25a8894e03857636
HristinaP06 <hristina_pirinova@mail.bg>
2025-03-19 00:00:44 +0200

    Created review controller in admin area

---
+++ FoodLoverGuide/Areas/Admin/Controllers/ReviewController.cs
@@ -0,0 +1,55 @@
+﻿using FoodLoverGuide.Core.IServices;
+using FoodLoverGuide.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodLoverGuide.Areas.Admin.Controllers
+{
+    public class ReviewController : BaseController
+    {
+        private readonly IReviewService reviewService;
+
+        public ReviewController(IReviewService reviewService)
+        {
+            this.reviewService = reviewService;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var list = await this.reviewService.GetAll().ToListAsync();
+            return View(list);
+        }
+
+        public IActionResult Create()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(Review review)
+        {
+            await this.reviewService.Add(review);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditAsync(Guid id)
+        {
+            var obj = await reviewService.GetById(id);
+            return View(obj);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Review review)
+        {
+            await this.reviewService.Update(review);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await this.reviewService.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
