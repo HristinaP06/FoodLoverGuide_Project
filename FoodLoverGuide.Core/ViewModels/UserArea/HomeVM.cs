667dda8082941c4f44cf068b1dd425355e18d09c
HristinaP06 <hristina_pirinova@mail.bg>
2025-03-23 23:13:26 +0200

    Renamed folder from User to UserArea

--- FoodLoverGuide.Core/ViewModels/UserArea/HomeVM.cs
+++ FoodLoverGuide.Core/ViewModels/UserArea/HomeVM.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FoodLoverGuide.Core.ViewModels.User
+namespace FoodLoverGuide.Core.ViewModels.UserArea
 {
     public class HomeVM
     {
