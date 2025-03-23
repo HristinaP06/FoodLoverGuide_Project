648f61370aa130cb4d943dc8f66161df83000c1d
HristinaP06 <hristina_pirinova@mail.bg>
2025-03-23 23:15:59 +0200

    Created users view and implemented functionality

---
+++ FoodLoverGuide.Core/ViewModels/User/UserDisplayVM.cs
@@ -0,0 +1,22 @@
+﻿using FoodLoverGuide.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodLoverGuide.Core.ViewModels.User
+{
+    public class UserDisplayVM
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+        public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
+    }
+}
---
+++ FoodLoverGuide/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,73 @@
+﻿using FoodLoverGuide.Core.IServices;
+using FoodLoverGuide.Core.ViewModels.User;
+using FoodLoverGuide.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodLoverGuide.Areas.Admin.Controllers
+{
+    public class UsersController : BaseController
+    {
+        private readonly UserManager<User> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public UsersController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var list =  this.userManager.Users.ToList();
+            var userVM = new List<UserVM>();
+
+            foreach (var user in list)
+            {
+                var roles = await userManager.GetRolesAsync(user);
+                var role = roles.FirstOrDefault();
+
+                userVM.Add(new UserVM
+                {
+                    Id = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.Email,
+                    Role = role,
+                });
+            }
+
+            return View(userVM);
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return NotFound();
+            }
+            
+            var user = this.userManager.Users.Where(u => u.Id == id.ToString()).First();
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var model = new UserDisplayVM
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Age = user.Age,
+                Reservations = user.Reservations,
+            };
+
+            return View(model);
+        }
+    }
+}
